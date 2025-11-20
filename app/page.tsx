519e56e2ad4a746c644545a3a022dc2c1adfa9bd
balbader <basilalbapro@gmail.com>
2025-11-20 07:45:34 +0100

    wip

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,11 @@
 import { HeroSection } from '@/components/home/hero-section';
-import { QuoteSection } from '@/components/home/quote-section';
 import { HowItWorksSection } from '@/components/home/how-it-works-section';
-import { CTASection } from '@/components/home/cta-section';
 
 export default function Home() {
 	return (
 		<div className="min-h-screen bg-white dark:bg-slate-950">
 			<HeroSection />
 			<HowItWorksSection />
-			<QuoteSection />
 		</div>
 	);
 }
