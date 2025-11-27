9a22d038c8502edb3e96e6b4d233d50c2cd2ebe0
balbader <basilalbapro@gmail.com>
2025-11-27 08:20:49 +0100

    fix env variables

--- env.ts
+++ env.ts
@@ -9,9 +9,9 @@ const envSchema = z.object({
 	KINDE_ISSUER_URL: z.string(),
 	KINDE_CLIENT_ID: z.string(),
 	KINDE_CLIENT_SECRET: z.string(),
-	KINDE_SITE_URI: z.string(),
-	KINDE_POST_LOGOUT_REDIRECT_URI: z.string(),
-	KINDE_POST_LOGIN_REDIRECT_URI: z.string(),
+	KINDE_SITE_URL: z.string(),
+	KINDE_POST_LOGOUT_REDIRECT_URL: z.string(),
+	KINDE_POST_LOGIN_REDIRECT_URL: z.string(),
 });
 
 const publicEnv: Record<string, string> = {
@@ -22,9 +22,9 @@ const publicEnv: Record<string, string> = {
 	KINDE_ISSUER_URL: process.env.KINDE_ISSUER_URL!,
 	KINDE_CLIENT_ID: process.env.KINDE_CLIENT_ID!,
 	KINDE_CLIENT_SECRET: process.env.KINDE_CLIENT_SECRET!,
-	KINDE_SITE_URI: process.env.KINDE_SITE_URI!,
-	KINDE_POST_LOGOUT_REDIRECT_URI: process.env.KINDE_POST_LOGOUT_REDIRECT_URI!,
-	KINDE_POST_LOGIN_REDIRECT_URI: process.env.KINDE_POST_LOGIN_REDIRECT_URI!,
+	KINDE_SITE_URL: process.env.KINDE_SITE_URL!,
+	KINDE_POST_LOGOUT_REDIRECT_URL: process.env.KINDE_POST_LOGOUT_REDIRECT_URL!,
+	KINDE_POST_LOGIN_REDIRECT_URL: process.env.KINDE_POST_LOGIN_REDIRECT_URL!,
 };
 
 export type EnvType = z.infer<typeof envSchema>;
