967fad20ec469cba47af4b7755339329fd427e4c
balbader <basilalbapro@gmail.com>
2025-12-01 10:27:49 +0100

    WIP

--- app/(pages)/home/page.tsx
+++ app/(pages)/home/page.tsx
@@ -2,14 +2,13 @@ import { HeroSection } from '@/components/home/HeroSection';
 import { FeaturesSection } from '@/components/home/FeaturesSection';
 import { HowItWorksSection } from '@/components/home/HowItWorksSection';
 import { AboutSection } from '@/components/home/AboutSection';
-import { Pricing } from '@/components/home/PricingSection';
+// import { Pricing } from '@/components/home/PricingSection';
 
 export default function Page() {
 	return (
 		<div className="min-h-screen bg-white dark:bg-slate-950">
 			<HeroSection />
 			<HowItWorksSection />
-			<Pricing />
 			<FeaturesSection />
 			<AboutSection />
 		</div>
